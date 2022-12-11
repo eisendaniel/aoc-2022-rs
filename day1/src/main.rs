465583e4d0ea8fe4817140c0e790cb38898f457e
eisendaniel <daniel.eisen@pm.me>
2022-12-12 10:26:10 +1300

    day1 compl

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,32 @@
+const _INPUT: &str = include_str!("input");
+const _EXAMPLE: &str = include_str!("example");
+
+fn _part1() -> u64 {
+    _INPUT
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .filter_map(|food| food.parse::<u64>().ok())
+                .sum()
+        })
+        .max()
+        .unwrap_or(0)
+}
+
+fn _part2() -> u64 {
+    let mut sums = _INPUT
+        .split("\n\n")
+        .map(|elf| {
+            elf.lines()
+                .filter_map(|food| food.parse::<u64>().ok())
+                .sum()
+        })
+        .collect::<Vec<u64>>();
+    sums.sort_unstable();
+    sums.iter().rev().take(3).sum()
+}
+
 fn main() {
-    let input = include_str!("example");
-    input.lines().for_each(|line| {
-        println!("{}", line);
-    });
+    println!("{}", _part1());
+    println!("{}", _part2());
 }
